427474b1a6efa67e1bd48591b35dfa5d7958dbd1
serenDave <1denrocks1@gmail.com>
2021-04-20 21:53:41 +0300

    prepared front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home";
-import NotFound from "./views/NotFound";
-import NavBar from "./components/Header/NavBar";
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <NavBar />
+    <>
       <Switch>
-        <Route exact path="/Home" component={Home} />
-        <Route exact path="/">
-          <Redirect to="/Home" />
-        </Route>
-        <Route component={NotFound}/>
+        {/* <Route exact path="/Home" component={Home} /> */}
       </Switch>
-    </div>
+    </>
   );
 }
 
